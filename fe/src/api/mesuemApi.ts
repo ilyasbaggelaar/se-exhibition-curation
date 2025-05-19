9170c847aa37fad7b1b8b8c30ab59ef29c1c5cb7
ilyasbaggelaar <ilyas.baggelaar@gmail.com>
2025-05-19 15:18:40 +0100

    -Pagination system developed, still needs improvement. Numeric pagination system should be developed alongside it.

--- fe/src/api/mesuemApi.ts
+++ fe/src/api/mesuemApi.ts
@@ -3,16 +3,21 @@ import axios from "axios";
 
 const BASE_URL = 'https://collectionapi.metmuseum.org/public/collection/v1';
 
-export const searchMesArtworks = async (query: string) => {
+export const searchMesArtworks = async (query: string, page: number, limit: number): Promise<any[]> => {
     const searchRes = await axios.get(`${BASE_URL}/search`, {
         params: {q: query, hasImages: true},
     });
 
     console.log(searchRes)
-    const objectIDs = searchRes.data.objectIDs?.slice(0, 10) || [];
+    const objectIDs = searchRes.data.objectIDs || [];
 
-    const promises = objectIDs.map(id =>
+    const start = (page - 1) * limit;
+
+    const paginatedIDs = objectIDs.slice(start, start + limit);
+
+    const promises = paginatedIDs.map((id: number) =>
          axios.get(`${BASE_URL}/objects/${id}`).then(res => res.data));
+
 
     return Promise.all(promises);
 
--- fe/src/pages/SearchPage.tsx
+++ fe/src/pages/SearchPage.tsx
@@ -1,29 +1,57 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { searchMesArtworks } from "../api/mesuemApi";
 
 function SearchPage() {
 
     const [artworks, setArtworks] = useState<any[]>([]);
     const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState("plato");
+    const [page, setPage] = useState(1);
+    const itemsPerPage = 10;
 
     useEffect(() => {
         const fetchArt = async () => {
             try {
-                const results = await searchMesArtworks('plato');
+                setLoading(true)
+                const results = await searchMesArtworks(search, page, itemsPerPage);
                 setArtworks(results);
+                setLoading(false);
             } catch(err) {
                 console.error(err);
-            } finally {
-                setLoading(false);
             }
         }
 
         fetchArt()
-    }, []);
+    }, [search, page]);
 
     return (
         <div>
-            <h2>Search Results...</h2>
+
+            <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="search artworks..."
+            />
+
+            <button onClick={() => setPage((p) => Math.max(p - 1, 1))} disabled={page === 1}>
+                Previous
+            </button>
+
+            <button onClick={() => setPage((p) => p + 1)}>Next</button>
+
+            {loading ? <p>loading...</p> : (
+                <div>
+                    {artworks.map((art) => (
+                        <div key={art.objectID}>
+                             <img src={art.primaryImageSmall} alt={art.title} />
+                             <h3>{art.title}</h3>
+                             <p>{art.artistDisplayName}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+            {/* <h2>Search Results...</h2>
             {loading ? (<p>Loading...</p>) : (
                 <ul>
                     {artworks.map(art => (
@@ -33,7 +61,7 @@ function SearchPage() {
                         </li>
                     ))}
                 </ul>
-            )}
+            )} */}
         </div>
     )
 
