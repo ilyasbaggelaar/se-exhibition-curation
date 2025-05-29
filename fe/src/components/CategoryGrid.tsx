9155bcdbdbfb53d577ffb9a607dc3f7610d1f422
ilyasbaggelaar <ilyas.baggelaar@gmail.com>
2025-05-29 11:33:36 +0100

    -Adjusted spacing on categories homepage

--- fe/src/components/CategoryGrid.tsx
+++ fe/src/components/CategoryGrid.tsx
@@ -85,7 +85,7 @@ export default function CategoryGrid() {
 			>
 				{showMore ? "Show Less" : "More Categories"}
 			</button>
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mb-16">
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mb-5">
 				{firstThree.map((cat) => (
 					<Link
 						key={cat.query}
